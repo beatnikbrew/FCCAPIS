15e408820cc7474cd359d6cc8850856c5db752fa
beatnikbrew <beatnikbrew@gmail.com>
2017-06-14 21:50:17 -0400

    now app is listening

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
-const express = require('express');
+var express = require('express');
 
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
 var app = express();
 
 app.get('/', (req, res) => {
   res.send('Here I am!');
+});
+
+app.listen(port, () => {
+  console.log("listening on " + port);
 });
