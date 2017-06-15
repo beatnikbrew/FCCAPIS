3e0d01ee2cbd5e47bd7677885677f575a0d66b65
beatnikbrew <beatnikbrew@gmail.com>
2017-06-14 22:26:31 -0400

    trying to parse date formats

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 var express = require('express');
-
-// set the port of our application
-// process.env.PORT lets the port be set by Heroku
+var moment = require('moment');
+// set the application port
+// process.env.PORT is whatever Heroku says the port should be
 var port = process.env.PORT || 8080;
+
 var app = express();
 
 app.get('/', (req, res) => {
-  res.send('Here I am!');
+  //Use moment to determine which format the date is in
+  date = moment(req);
+  if (isNaN(req)) {
+    console.log("The following is a natural language date" +date);
+  } else {console.log("Unix timestamp is" + date);}
 });
 
 app.listen(port, () => {
