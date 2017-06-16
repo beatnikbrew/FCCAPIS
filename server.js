ffce8b23041fcc57c8e2c35752324f58ad793e36
beatnikbrew <beatnikbrew@gmail.com>
2017-06-15 22:15:35 -0400

    type, why do I do this to myself

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('/:input', (req, res) => {
 });
 
 function dateParser(date) {
-  if (moment(date).isValid() || moment.unix(parseint(date)).isValid()) {
+  if (moment(date).isValid() || moment.unix(parseInt(date)).isValid()) {
     return moment(date);
   } else {return "Not a valid date";}
 }
